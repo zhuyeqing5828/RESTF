31aea593ec62ff895be435b1e35fea3a5f5690c7
zhuyeqing5828 <582845992@qq.com>
2018-08-20 11:03:36 +0800

    add gitignore

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 A java RESTful frame just for fun
 
 #### Mabe for springboot is batter
+#### thanks spring-guides for start
---
+++ src/main/boot/Init.java
@@ -0,0 +1,13 @@
+package boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Init {
+    public static void main(String[] args){
+        SpringApplication.run(Init.class,args);
+    }
+
+}
+    
